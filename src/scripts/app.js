a9380783691e2c5c82aa134c488e51960fbd11df
iFiring <wchuan@drips.cn>
2018-01-19 18:31:23 +0800

    add method for <a> active

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,41 @@
+//初始化主页
+$(document).ready(function () {
+    $.get("./README.md",
+        function (data) {
+            $('#content').html(marked(data));
 
-$('#nav-ul').on(
-    'click',function (event) {
+            $('#content code').map(function() {
+                Prism.highlightElement(this);
+            });
+        }
+    );
+});
+
+//为菜单列表绑定事件
+$('#nav-ul').on('click',
+    function (event) {
         //隐藏下拉框
         if(window.innerWidth<768){
             $('#collapse-target').collapse('hide');
         }
 
-        var url = event.target.getAttribute('href');
+        //移除所有.active并添加对应的.active
+        $('#nav-ul li a').removeClass('active');
+        $(event.target).addClass('active');
+
+        //获取目标元素的href属性值
+        var url = event.target.getAttribute('href'); //==$(event.target).attr('href');
         var source;
 
-        //-1 的Boolean值是True.
+        //计算文件正确的url值.
+        // -1 的Boolean值是True.
         if (url.search("README")+1) {
             source = "./"+url.slice(1)+".md";
         } else {
             source = "./docs/"+url.slice(1)+".md";
         }
+
+        //获取同源的markdown文件并解析
         $.get(
             source,function (data) {
                 $('#content').html(marked(data));
@@ -26,13 +47,3 @@ $('#nav-ul').on(
         )
     }
 )
-$(document).ready(function () {
-    $.get("./README.md",function (data) {
-            $('#content').html(marked(data));
-
-            $('#content code').map(function() {
-                Prism.highlightElement(this);
-            });
-        }
-    );
-});
